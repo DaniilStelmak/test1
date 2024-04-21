5a62fe9f394899d44bec4174648482018ce5379c
DaniilStelmak <154973215+DaniilStelmak@users.noreply.github.com>
2024-04-22 00:47:56 +0300

    myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+
 input_dir="$1"
 output_dir="$2"
+
 mkdir -p "$output_dir"
+
 function copy_files {
-for file in "$1"/*; do
+    for file in "$1"/*; do
         if [ -d "$file" ]; then
             copy_files "$file"
-elif [ -f "$file" ]; then
+        elif [ -f "$file" ]; then
             new_file="$output_dir/$(basename "$file")"
             if [ -f "$new_file" ]; then
                 base="${file%.*}"
                 ext="${file##*.}"
                 i=1
-while [ -f "$new_file" ]; do
+                while [ -f "$new_file" ]; do
                     new_file="$output_dir/$(basename "$base")_$i.$ext"
                     let i++
                 done
@@ -21,6 +24,5 @@ while [ -f "$new_file" ]; do
         fi
     done
 }
+
 copy_files "$input_dir"
-
-
